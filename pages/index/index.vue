b2991395a6fe099063fc60baca8041a3e2fec996
ilss <375259904@qq.com>
2021-03-09 09:18:09 +0800

    基础代码

---
+++ pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <view>
+    <!-- <web-view style="width: 100vw; height: 100vh;" src="/hybrid/html/index.html"></web-view> -->
+    <unity src="/hybrid/html/Build/WebGL.json"  unityLoader="/hybrid/html/Build/UnityLoader.js"></unity>
+  </view>
+</template>
+
+<script>
+import Unity from 'vue-unity-webgl';
+export default {
+  components: {
+    Unity
+  },
+  data() {
+    return {
+      title: 'Hello'
+    };
+  },
+  onLoad() {},
+  methods: {}
+};
+</script>
+
+<style>
+/* @media screen and (max-width: 640px) {
+   #unity-container {
+      display: none;
+   } 
+} */
+
+</style>
